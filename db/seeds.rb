70e1b7a9b6f4803a990cd1d631bbe245ff8c3ffb
James Sapp <jameseisapp@gmail.com>
2021-01-13 21:59:11 -0600

    commit tree efcf6f4ff75648be510125f6c611a6f8ec694cdc

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,9 @@
 # Add seed data here. Seed your database with `rake db:seed`
 sophie = Owner.create(name: "Sophie")
+james = Owner.create(name: "James")
+max = Owner.create(name: "Max")
 Pet.create(name: "Maddy", owner: sophie)
 Pet.create(name: "Nona", owner: sophie)
+Pet.create(name: "Ivan", owner: james)
+Pet.create(name: "Harley", owner: max)
+Pet.create(name: "Fletcher", owner: james)
